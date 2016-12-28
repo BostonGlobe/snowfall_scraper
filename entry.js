3c86d60bfbf9ff86732f0b5a32c473702794d64d
Gabriel Florit <gabrielflorit@gmail.com>
2016-12-28 18:26:18 -0500

    Move index.js to entry.js and viceversa

--- entry.js
+++ entry.js
@@ -1,2 +1,14 @@
-require('babel-register')
-require('./index.js')
+import getReports from './scrapers/getReports.js'
+import shell from 'shelljs'
+
+getReports()
+	.then(results => {
+
+		console.log(JSON.stringify(results, null, 2))
+
+	})
+	.catch(error => {
+
+		shell.exec('echo "' + error + '" | mail -s "error in snowfall_scraper." gabriel.florit@globe.com')
+
+	})
--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-import getReports from './scrapers/getReports.js'
-import shell from 'shelljs'
-
-getReports()
-	.then(results => {
-
-		console.log(JSON.stringify(results, null, 2))
-
-	})
-	.catch(error => {
-
-		shell.exec('echo "' + error + '" | mail -s "error in snowfall_scraper." gabriel.florit@globe.com')
-
-	})
+require('babel-register')
+require('./entry.js')
